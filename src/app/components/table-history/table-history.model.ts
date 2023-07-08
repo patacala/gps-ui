0b2a8331fa71e3e8a2600284c4824d058b8e5df8
ajcnazar <ajcnazar@gmail.com>
2023-07-08 10:02:23 -0500

    dirección en detalle de vehiculo

--- src/app/components/table-history/table-history.model.ts
+++ src/app/components/table-history/table-history.model.ts
@@ -2,6 +2,7 @@ export interface HistoryLoc {
     delonuid: number,
     delolati: number;
     delolong: number;
+    delodire: string;
     delospee: string;
     delotime: string;
     delofesi: string;
